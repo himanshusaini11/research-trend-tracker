c4a45951693bacec1c35ab6f8e920f7e9c1ae34e
Himanshu Saini <sainih5@outlook.com>
2026-03-12 16:23:55 -0400

    feat: implement app/core module with DB verification

---
+++ alembic/env.py
@@ -0,0 +1,46 @@
+from logging.config import fileConfig
+
+from sqlalchemy import pool
+
+from alembic import context
+
+from app.core.database import get_sync_engine
+from app.core.models import Base
+
+config = context.config
+
+if config.config_file_name is not None:
+    fileConfig(config.config_file_name)
+
+target_metadata = Base.metadata
+
+
+def run_migrations_offline() -> None:
+    from app.core.config import settings
+
+    context.configure(
+        url=settings.postgres_dsn_sync,
+        target_metadata=target_metadata,
+        literal_binds=True,
+        dialect_opts={"paramstyle": "named"},
+    )
+    with context.begin_transaction():
+        context.run_migrations()
+
+
+def run_migrations_online() -> None:
+    connectable = get_sync_engine()
+
+    with connectable.connect() as connection:
+        context.configure(
+            connection=connection,
+            target_metadata=target_metadata,
+        )
+        with context.begin_transaction():
+            context.run_migrations()
+
+
+if context.is_offline_mode():
+    run_migrations_offline()
+else:
+    run_migrations_online()
